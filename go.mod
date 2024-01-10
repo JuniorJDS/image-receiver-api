5df9b3bffd648294beec6438d909db13a574a57b
Junior Silva <juniorjds2015@gmail.com>
2024-01-10 18:57:17 -0300

    feat: added aws client connection

--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module image-receiver-api
 go 1.21.5
 
 require (
-	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/gofiber/fiber/v2 v2.52.0
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/aws/aws-sdk-go v1.49.17 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
---
+++ infra/aws/setup.go
@@ -0,0 +1,31 @@
+package aws
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+var client *session.Session
+
+func Client() *session.Session {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-west-2"),
+		Credentials: credentials.NewSharedCredentials("", "dev-go-account"),
+	},
+	)
+	if err != nil {
+		log.Printf("Error to connect to aws: %s\n", err.Error())
+	}
+
+	return sess
+}
+
+func GetClientAWS() *session.Session {
+	if client == nil {
+		client = Client()
+	}
+	return client
+}
